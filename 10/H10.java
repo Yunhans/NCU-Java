0cebc530af0080a65c73081c4f5c840829022019
Yunhans <91072133+Yunhans@users.noreply.github.com>
2022-05-26 12:22:17 +0800

    2022/05/26

--- 10/H10.java
+++ 10/H10.java
@@ -15,17 +15,14 @@ abstract class C {
 }
 
 abstract class B extends C {
-    public void m1(){
-        System.out.println("B's m1()");
+    public void m2(){
+        System.out.println("B's m2()");
     }
 }
 
 class A extends B {
     public void m1(){
-        System.out.println("A's m1()");
-    }
-    public void m2(){
-        System.out.println("A's m2");
+        System.out.println("A's m1");
     }
     public final void m3(){
         System.out.println("A's m3");
